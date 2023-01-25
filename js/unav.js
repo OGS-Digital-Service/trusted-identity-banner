cf7030c155d4b3809740bc9949546d6de9611e0c
Jeff Knaack <58082485+knaackbuilt@users.noreply.github.com>
2023-01-25 12:16:55 -0500

    tweak js to listen at mobile for area click

--- js/unav.js
+++ js/unav.js
@@ -1,14 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const unavbanner = document.getElementById('unav-banner');
     const unavbutton = document.getElementById('trust-disclose');
     const disclosure = document.getElementById('offical-site-disclosure');
     const unavarrow = document.getElementById('unav-arrow');
 
     unavbutton.addEventListener("click", showdisclouse) 
+    
+    if (window.innerWidth < 768){
+        unavbanner.addEventListener("click", showdisclouse) 
+    }
+
     function showdisclouse () {
         disclosure.classList.toggle('unav-hidden');
         disclosure.classList.toggle('unav-flex');
         unavarrow.classList.toggle('unav-rotate-180');
 
     }
-    return console.log(unavbutton,disclosure)
 })
